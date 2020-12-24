11e691e4d2360577ebf0f3b3168dbc191bf1a992
ChenCMD <scnmed.g@gmail.com>
2020-12-24 19:01:37 +0900

    :bug: フレーム間の補完の選択肢がおかしいのを修正

--- src/types/AnimationMcmeta.ts
+++ src/types/AnimationMcmeta.ts
@@ -20,8 +20,8 @@ export function createAnimationMcmeta(interpolate: QuickPickItemHasId<BooleanLik
 export function getInterpolateMap(): Map<BooleanLikeString, string> {
     const res = new Map<BooleanLikeString, string>();
 
-    res.set('true', '単体テクスチャ');
-    res.set('false', 'アニメーションテクスチャ');
+    res.set('true', 'interpolate(フレーム間補完)を有効にする');
+    res.set('false', 'interpolate(フレーム間補完)を無効にする');
 
     return res;
 }
