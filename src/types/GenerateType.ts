2b6de58fd4f594cb69deab520449f5871b48a9bb
ChenCMD <scnmed.g@gmail.com>
2021-01-03 21:26:25 +0900

    :children_crossing: 質問の項目をわかりやすく

--- src/types/GenerateType.ts
+++ src/types/GenerateType.ts
@@ -6,10 +6,10 @@ export type GenerateType = 'single' | 'animation' | 'vanilla'/* | '3D'*/;
 export function getGenTypeMap(): Map<string, AbstractNode> {
     const res = new Map<string, AbstractNode>();
 
-    res.set('単体テクスチャ', new Single2DGenNode());
+    res.set('非アニメーションテクスチャ', new Single2DGenNode());
     res.set('アニメーションテクスチャ', new Animated2DGenNode());
     res.set('バニラテクスチャ', new VanillaGenNode());
-    res.set('3Dモデル', new Single3DGenNode());
+    res.set('非アニメーション3Dモデル', new Single3DGenNode());
 
     return res;
 }
